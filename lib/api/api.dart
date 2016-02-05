9d05a9ca6236bc1e8bae545a35a1cc5973075eb6
Kendal Harland <kendaljharland@gmail.com>
2016-02-05 10:02:21 -0800

    add method to enforce var types and implement sendDocument, sendPhoto, sendAudio and forwardMessage. Alphabetize Objects

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,12 +4,19 @@ import 'dart:async';
 
 import 'package:telegram_bot/rpc.dart';
 import 'package:telegram_bot/api/objects.dart';
+
 export 'package:telegram_bot/api/objects.dart';
+
+void _forceType(object, List<Type> types) {
+  if (!types.any((Type T) => object.runtimeType == T)) {
+    throw new TypeError();
+  }
+}
 
 /// A simple method for testing your bot's auth token. Requires no parameters.
 /// Returns basic information about the bot in form of a User object.
 Future<User> getMe(String token) =>
-    requestMethod('getMe', token).then((userMap) => new User(userMap));
+    requestMethod('getMe', token).then(User.create);
 
 /// [offset] is an identifier of the first update to be returned. Must be
 /// greater by one than the highest among the identifiers of previously received
@@ -23,12 +30,15 @@ Future<User> getMe(String token) =>
 /// [timeout] is the timeout in seconds for long polling. Defaults to 0, i.e.
 /// usual short polling
 Future<List<Update>> getUpdates(String token,
-    {int offset, int limit, int timeout}) {
-  var args = _removeNullKeys(
-      {'offset': '$offset', 'limit': '$limit', 'timeout': '$timeout'});
+    {int offset, int limit: 100, int timeout: 0}) {
+  var args = <String, String>{'limit': '$limit', 'timeout': '$timeout'};
+
+  if (offset != null) {
+    args['offset'] = '$offset';
+  }
 
   return requestMethod('getUpdates', token, args: args)
-      .then((updateMaps) => updateMaps.map((map) => new Update(map)).toList());
+      .then((updateMaps) => updateMaps.map(Update.create).toList());
 }
 
 /// Use this method to send text messages. On success, the sent Message is
@@ -36,27 +46,114 @@ Future<List<Update>> getUpdates(String token,
 ///
 /// Todo(kharland): Support replyMarkup optional parameter
 Future<Message> sendMessage(String token, int chatId, String text,
-    {String parseMode,
-    bool disableWebPagePreview,
+    {String parseMode: 'Markdown',
+    bool disableWebPagePreview: false,
     int replyToMessageId}) async {
-  var args = _removeNullKeys({
+  var args = {
     'chat_id': '$chatId',
     'text': text,
     'parse_mode': parseMode,
     'disable_web_page_preview': '$disableWebPagePreview',
-    'reply_to_message_id': '$replyToMessageId',
-  });
+  };
+
+  if (replyToMessageId != null) {
+    args['reply_to_message_id'] = '$replyToMessageId';
+  }
 
   var messageMap = await requestMethod('sendMessage', token, args: args);
   return new Message(messageMap);
 }
 
-Map<String, String> _removeNullKeys(Map<String, Object> fields) {
-  var map = <String, String>{};
+/// Use this method to forward messages of any kind. On success, the sent
+/// Message is returned.
+Future<Message> forwardMessage(
+    String token, int chatId, int fromChatId, int messageId) {
+  var args = {
+    'chat_id': '$chatId',
+    'from_chat_id': '$fromChatId',
+    'message_id': '$messageId'
+  };
 
-  fields.keys.forEach((String key) {
-    if (fields[key] != null) map[key] = fields[key];
-  });
+  return requestMethod('forwardMessage', token, args: args)
+      .then(Message.create);
+}
 
-  return map;
+/// Use this method to send photos. On success, the sent Message is returned.
+Future<Message> sendPhoto(String token, int chatId, dynamic photo,
+    {String caption, int replyToMessageId, dynamic replyMarkup}) {
+  _forceType(photo, [String, InputFile]);
+  var args = {'chat_id': '$chatId', 'photo': '$photo',};
+
+  if (caption != null) {
+    args['caption'] = caption;
+  }
+  if (replyToMessageId != null) {
+    args['reply_to_message_id'] = '$replyToMessageId';
+  }
+  if (replyMarkup != null) {
+    _forceType(
+        replyMarkup, [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply]);
+    args['reply_markup'] = '$replyMarkup';
+  }
+
+  return requestMethod('sendPhoto', token, args: args).then(Message.create);
+}
+
+/// Use this method to send audio files, if you want Telegram clients to display
+/// them in the music player. Your audio must be in the .mp3 format. On
+/// success, the sent Message is returned. Bots can currently send audio files
+/// of up to 50 MB in size, this limit may be changed in the future.
+
+/// For backward compatibility, when the fields title and performer are both
+/// empty and the mime-type of the file to be sent is not audio/mpeg, the file
+/// will be sent as a playable voice message. For this to work, the audio must
+/// be in an .ogg file encoded with OPUS. This behavior will be phased out in
+/// in the future. For sending voice messages, use the sendVoice method instead.
+Future<Message> sendAudio(String token, int chatId, dynamic audio,
+    {int duration,
+    String performer,
+    String title,
+    int replyToMessageId,
+    dynamic replyMarkup}) {
+  _forceType(audio, [String, InputFile]);
+  var args = {'chat_id': '$chatId', 'audio': '$audio',};
+
+  if (duration != null) {
+    audio['duration'] = '$duration';
+  }
+  if (performer != null) {
+    audio['performer'] = performer;
+  }
+  if (title != null) {
+    audio['title'] = title;
+  }
+  if (replyToMessageId != null) {
+    args['reply_to_message_id'] = '$replyToMessageId';
+  }
+  if (replyMarkup != null) {
+    _forceType(
+        replyMarkup, [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply]);
+    args['reply_markup'] = '$replyMarkup';
+  }
+
+  return requestMethod('sendAudio', token, args: args).then(Message.create);
+}
+
+/// Use this method to send general files. On success, the sent Message is
+/// returned. Bots can currently send files of any type of up to 50 MB in size,
+/// this limit may be changed in the future.
+Future<Message> sendDocument(String token, int chatId, document,
+    {int replyToMessageId, dynamic replyMarkup}) {
+  _forceType(document, [String, InputFile]);
+  var args = {'chat_id': '$chatId', 'document': '$document'};
+
+  if (replyToMessageId != null) {
+    args['reply_to_message_id'] = '$replyToMessageId';
+  }
+  if (replyMarkup != null) {
+    _forceType(
+        replyMarkup, [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply]);
+    args['reply_markup'] = '$replyMarkup';
+  }
+  return requestMethod('sendDocument', token, args: args).then(Message.create);
 }
--- lib/api/objects.dart
+++ lib/api/objects.dart
@@ -13,43 +13,96 @@ class APIObject {
   operator []=(String key, Object value) => _map[key] = value;
 }
 
-class User extends APIObject {
-  User(Map<String, Object> map) : super(map);
+class Audio extends APIObject {
+  Audio(Map<String, Object> map) : super(map);
 
-  int get id => super['id'];
-  String get firstName => super['first_name'];
-  String get lastName => super['last_name'];
-  String get username => super['username'];
+  String get fileId => super['file_id'];
+  int get duration => super['duration'];
+  String get performer => super['performer'];
+  String get title => super['title'];
+  String get mimeType => super['mime_type'];
+  int get fileSize => super['file_size'];
 }
 
-class UserProfilePhotos extends APIObject {
-  UserProfilePhotos(Map<String, Object> map) : super(map);
-
-  int get totalCount => super['total_count'];
-  List<PhotoSize> get photos => super['photos'];
+class Chat extends APIObject {
+  Chat(Map<String, Object> map) : super(map);
 }
 
-class Update extends APIObject {
-  Update(Map<String, Object> map) : super(map) {
-    if (super['message'] != null) {
-      super['message'] = new Message(super['message']);
-    }
-    if (super['inline_query'] != null) {
-      super['inline_query'] = new InlineQuery(super['inline_query']);
-    }
-    if (super['chosen_inline_result'] != null) {
-      super['chosen_inline_result'] =
-          new ChosenInlineResult(super['chosen_inline_result']);
+class ChosenInlineResult extends APIObject {
+  ChosenInlineResult(Map<String, Object> map) : super(map) {
+    if (super['from'] != null) {
+      super['from'] = new User(super['from']);
     }
   }
 
-  int get id => super['update_id'];
-  Message get message => super['message'];
-  InlineQuery get inlineQuery => super['inline_query'];
-  ChosenInlineResult get chosenInlineResult => super['chosen_inline_result'];
+  String get resultId => super['result_id'];
+  User get from => super['from'];
+  String get query => super['query'];
+}
+
+class Contact extends APIObject {
+  Contact(Map<String, Object> map) : super(map);
+
+  String get phoneNumber => super['phone_number'];
+  String get firstName => super['first_name'];
+  String get lastName => super['last_name'];
+  int get userId => super['user_id'];
+}
+
+class Document extends APIObject {
+  Document(Map<String, Object> map) : super(map) {
+    if (super['thumb'] != null) {
+      super['thumb'] = new PhotoSize(super['thumb']);
+    }
+  }
+
+  String get fileId => super['file_id'];
+  PhotoSize get thumb => super['thumb'];
+  String get fileName => super['file_name'];
+  String get mimeType => super['mime_type'];
+  int get fileSize => super['file_size'];
+}
+
+class File extends APIObject {
+  File(Map<String, Object> map) : super(map);
+
+  String get fileId => super['file_id'];
+  int get fileSize => super['file_size'];
+  String get filePath => super['file_path'];
+}
+
+class ForceReply extends APIObject {
+  ForceReply(Map<String, Object> map) : super(map);
+
+  bool get forceReply => super['force_reply'];
+  bool get selective => super['selective'];
+}
+
+class InlineQuery extends APIObject {
+  InlineQuery(Map<String, Object> map) : super(map) {
+    if (super['from'] != null) {
+      super['from'] = new User(super['from']);
+    }
+  }
+
+  String get id => super['id'];
+  User get from => super['from'];
+  String get query => super['query'];
+  String get offset => super['offset'];
+}
+
+class InputFile {}
+
+class Location extends APIObject {
+  Location(Map<String, Object> map) : super(map);
+
+  double get longitude => super['longitude'];
+  double get latitude => super['latitude'];
 }
 
 class Message extends APIObject {
+  static Message create(Map<String, Object> map) => new Message(map);
+
   Message(Map<String, Object> map) : super(map) {
     if (super['from'] != null) {
       super['from'] = new User(super['from']);
@@ -121,100 +174,6 @@ class Message extends APIObject {
   int get migrateFromChatId => super['migrate_from_chat_id'];
 }
 
-class Voice extends APIObject {
-  Voice(Map<String, Object> map) : super(map);
-
-  String get fileId => super['file_id'];
-  int get duration => super['duration'];
-  String get mimeType => super['mime_type'];
-  int get fileSize => super['file_size'];
-}
-
-class Contact extends APIObject {
-  Contact(Map<String, Object> map) : super(map);
-
-  String get phoneNumber => super['phone_number'];
-  String get firstName => super['first_name'];
-  String get lastName => super['last_name'];
-  int get userId => super['user_id'];
-}
-
-class Location extends APIObject {
-  Location(Map<String, Object> map) : super(map);
-
-  double get longitude => super['longitude'];
-  double get latitude => super['latitude'];
-}
-
-class File extends APIObject {
-  File(Map<String, Object> map) : super(map);
-
-  String get fileId => super['file_id'];
-  int get fileSize => super['file_size'];
-  String get filePath => super['file_path'];
-}
-
-class InlineQuery extends APIObject {
-  InlineQuery(Map<String, Object> map) : super(map) {
-    if (super['from'] != null) {
-      super['from'] = new User(super['from']);
-    }
-  }
-
-  String get id => super['id'];
-  User get from => super['from'];
-  String get query => super['query'];
-  String get offset => super['offset'];
-}
-
-class ChosenInlineResult extends APIObject {
-  ChosenInlineResult(Map<String, Object> map) : super(map) {
-    if (super['from'] != null) {
-      super['from'] = new User(super['from']);
-    }
-  }
-
-  String get resultId => super['result_id'];
-  User get from => super['from'];
-  String get query => super['query'];
-}
-
-class Chat extends APIObject {
-  Chat(Map<String, Object> map) : super(map);
-}
-
-class Audio extends APIObject {
-  Audio(Map<String, Object> map) : super(map);
-
-  String get fileId => super['file_id'];
-  int get duration => super['duration'];
-  String get performer => super['performer'];
-  String get title => super['title'];
-  String get mimeType => super['mime_type'];
-  int get fileSize => super['file_size'];
-}
-
-class Document extends APIObject {
-  Document(Map<String, Object> map) : super(map) {
-    if (super['thumb'] != null) {
-      super['thumb'] = new PhotoSize(super['thumb']);
-    }
-  }
-
-  String get fileId => super['file_id'];
-  PhotoSize get thumb => super['thumb'];
-  String get fileName => super['file_name'];
-  String get mimeType => super['mime_type'];
-  int get fileSize => super['file_size'];
-}
-
-class ForceReply extends APIObject {
-  ForceReply(Map<String, Object> map) : super(map);
-
-  bool get forceReply => super['force_reply'];
-  bool get selective => super['selective'];
-}
-
 class PhotoSize extends APIObject {
   PhotoSize(Map<String, Object> map) : super(map);
 
@@ -224,19 +183,19 @@ class PhotoSize extends APIObject {
   int get fileSize => super['file_size'];
 }
 
+class ReplyKeyboardHide extends APIObject {
+  ReplyKeyboardHide(Map<String, Object> map) : super(map);
+
+  bool get hideKeyboard => super['hide_keyboard'];
+  bool get selective => super['selective'];
+}
+
 class ReplyKeyboardMarkup extends APIObject {
   ReplyKeyboardMarkup(Map<String, Object> map) : super(map);
 
   List<List<String>> get keyboard => super['keyboard'];
   bool get resizeKeyboard => super['resize_keyboard'];
   bool get oneTimeKeyboard => super['one_time_keyboard'];
-  bool get selective => super['selective'];
-}
-
-class ReplyKeyboardHide extends APIObject {
-  ReplyKeyboardHide(Map<String, Object> map) : super(map);
-
-  bool get hideKeyboard => super['hide_keyboard'];
   bool get selective => super['selective'];
 }
 
@@ -254,6 +213,46 @@ class Sticker extends APIObject {
   int get fileSize => super['file_size'];
 }
 
+class Update extends APIObject {
+  static Update create(Map<String, Object> map) => new Update(map);
+
+  Update(Map<String, Object> map) : super(map) {
+    if (super['message'] != null) {
+      super['message'] = new Message(super['message']);
+    }
+    if (super['inline_query'] != null) {
+      super['inline_query'] = new InlineQuery(super['inline_query']);
+    }
+    if (super['chosen_inline_result'] != null) {
+      super['chosen_inline_result'] =
+          new ChosenInlineResult(super['chosen_inline_result']);
+    }
+  }
+
+  int get id => super['update_id'];
+  Message get message => super['message'];
+  InlineQuery get inlineQuery => super['inline_query'];
+  ChosenInlineResult get chosenInlineResult => super['chosen_inline_result'];
+}
+
+class User extends APIObject {
+  static User create(Map<String, Object> map) => new User(map);
+
+  User(Map<String, Object> map) : super(map);
+
+  int get id => super['id'];
+  String get firstName => super['first_name'];
+  String get lastName => super['last_name'];
+  String get username => super['username'];
+}
+
+class UserProfilePhotos extends APIObject {
+  UserProfilePhotos(Map<String, Object> map) : super(map);
+
+  int get totalCount => super['total_count'];
+  List<PhotoSize> get photos => super['photos'];
+}
+
 class Video extends APIObject {
   Video(Map<String, Object> map) : super(map) {
     if (super['thumb'] != null) {
@@ -266,6 +265,15 @@ class Video extends APIObject {
   int get height => super['height'];
   int get duration => super['duration'];
   PhotoSize get thumb => super['thumb'];
+  String get mimeType => super['mime_type'];
+  int get fileSize => super['file_size'];
+}
+
+class Voice extends APIObject {
+  Voice(Map<String, Object> map) : super(map);
+
+  String get fileId => super['file_id'];
+  int get duration => super['duration'];
   String get mimeType => super['mime_type'];
   int get fileSize => super['file_size'];
 }
