0b21fce8b25a06950d9387c73e2082320f9a9be5
Kendal Harland <kendaljharland@gmail.com>
2016-02-05 18:30:32 -0800

    move rpc.dart into api/

---
+++ lib/api/rpc.dart
@@ -0,0 +1,39 @@
+library telegram_bot.rpc;
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const API_HOST = 'api.telegram.org';
+const DEFAULT_SCHEME = 'http';
+
+Uri buildUri(String token, String apiMethod,
+        {Map<String, String> args,
+        String scheme: DEFAULT_SCHEME,
+        String host: API_HOST}) =>
+    new Uri(
+        scheme: scheme,
+        host: host,
+        path: 'bot$token/$apiMethod',
+        queryParameters: args);
+
+Future<Map<String, Object>> requestUri(Uri uri) async {
+  var response = await http.get('$uri');
+  var apiResponse;
+  try {
+    apiResponse = new JsonDecoder().convert(response.body);
+  } catch (e) {
+    stderr.write('[telegram_bot:requestUri] $e');
+  }
+
+  if (apiResponse['ok']) {
+    return apiResponse['result'];
+  } else {
+    throw "request for $uri returned 'ok'=false";
+  }
+}
+
+Future<Map<String, Object>> requestMethod(String method, String token,
+        {Map<String, String> args: const {}}) =>
+    requestUri(buildUri(token, method, args: args));
